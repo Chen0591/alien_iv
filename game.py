e192d68258c88a2409e7ab94a115295822549967
chen0591 <1160111713@qq.com>
2024-07-29 15:29:10 +0800

    “更新飞船控制逻辑，修复部分Bug。”

--- game.py
+++ game.py
@@ -1,7 +1,9 @@
 import sys
 import pygame
 from settings import Settings
-import game
+from ship import Ship
+import game_functions as gf
+
 
 
 def run_game():
@@ -10,17 +12,13 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    bg_color = (230, 230, 230)
+    # 创造飞船实例
+    ship = Ship(ai_settings, screen)
     # 开始游戏的主循环
     while True:
-        # 每次循环时都重绘屏幕
-        screen.fill(ai_settings.bg_color)
-        # 监督键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        # 让最近绘制的屏幕可见
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings,screen, ship)
 
 
 run_game()
